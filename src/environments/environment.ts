18ec30d5467168ff579b20670ae28e4cbe2cfca8
markkollar <mark.kollar@splendex.io>
2021-03-24 18:07:52 +0100

    initialize firebase and firestore

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyBHoRoQ_Sz3o25FsxiEzf_Qx_7lo0XSfYo",
+    authDomain: "szakdolgozat-72420.firebaseapp.com",
+    projectId: "szakdolgozat-72420",
+    storageBucket: "szakdolgozat-72420.appspot.com",
+    messagingSenderId: "852862388106",
+    appId: "1:852862388106:web:786a5e902cd019da82ea73",
+    measurementId: "G-D9JMMSY4X9"
+  }
 };
 
 /*
